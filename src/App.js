d0e5480fcb95eac353055a7f00758d38684f2649
RickardssonJ <rickardssonjimmy@gmail.com>
2022-04-29 11:03:10 +0200

    added react to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { useState } from 'react';
-import { BillInputFields } from './components/BillInputFields';
-import { SalaryInputs } from './components/SalaryInputs';
-import { TotalSumContext } from './context/TotalSumContext'
-import { StyledAppWrapper } from './Styles/Styling';
-
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { BillInputFields } from "./components/BillInputFields";
+import { SalaryInputs } from "./components/SalaryInputs";
+import { TotalSumContext } from "./context/TotalSumContext";
+import { StyledAppWrapper } from "./Styles/Styling";
 
 function App() {
-  const [totalBillSum, setTotalBillSum] = useState()
+  const [totalBillSum, setTotalBillSum] = useState();
 
   return (
     <>
