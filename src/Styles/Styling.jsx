1ed18d2caf93fe052639f31ff4012a6ad3b9d016
RickardssonJ <rickardssonjimmy@gmail.com>
2022-05-28 12:45:08 +0200

    minor changes

--- src/Styles/Styling.jsx
+++ src/Styles/Styling.jsx
@@ -1,15 +1,15 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const StyledAppWrapper = styled.div``
+export const StyledAppWrapper = styled.div``;
 
 export const StyledBillsWrapper = styled.div`
   display: flex;
   background-color: lightblue;
-`
+`;
 
 export const StyledBillsDiv = styled.div`
   margin: 0 12rem 0 1rem;
-`
+`;
 
 export const StyledBillsInputDiv = styled.div`
   padding: 1rem;
@@ -18,7 +18,7 @@ export const StyledBillsInputDiv = styled.div`
   form {
     display: flex;
   }
-`
+`;
 
 export const StyledSalaryInputDiv = styled.div`
   padding: 1rem;
@@ -27,15 +27,15 @@ export const StyledSalaryInputDiv = styled.div`
   form {
     display: flex;
   }
-`
+`;
 
 export const StyledP = styled.p`
   margin-top: 0;
-`
+`;
 
 export const StyledPriceWrapper = styled.div`
   display: flex;
-`
+`;
 
 export const StyledButton = styled.button`
   border: none;
@@ -47,17 +47,17 @@ export const StyledButton = styled.button`
   :hover {
     background-color: #f83535;
   }
-`
+`;
 
 export const StyledH3 = styled.h3`
   color: red;
-`
+`;
 
 export const StyledSpan = styled.span`
   color: #0019fb;
   font-size: 1.2rem;
   font-weight: bold;
-`
+`;
 export const StyledCountButton = styled.button`
   margin: 1rem 0 1rem 0;
   padding: 0.3rem 0.6rem;
@@ -69,13 +69,13 @@ export const StyledCountButton = styled.button`
   :hover {
     background-color: #f69527;
   }
-`
+`;
 export const StyledSalaryDiv = styled.div`
   padding: 1rem;
-`
+`;
 export const StyledInputField = styled.input`
   border-radius: 5px;
   border: none;
   padding: 0.1rem;
   margin-right: 1rem;
-`
+`;
